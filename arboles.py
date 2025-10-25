65fcedba4dd7efa713ed0792373d5f95f335d5a0
Guillermo Rivas <rivas.guillermo6426@gmail.com>
2025-10-25 09:37:19 -0600

    Representación lista
    Pasa los tests correctamente

--- arboles.py
+++ arboles.py
@@ -2,6 +2,7 @@ class Arbol:
     """Clase para representar árboles binarios recursivos. La variable 'raiz'
     es un elemento, 'izquierdo' es un árbol y 'derecho' es un árbol.
     """
+
     def __init__(self, raiz=None, izquierdo=None, derecho=None):
         """Constructor de la clase. Si raiz es None construye un árbol binario
         vacío, en otro caso, asigna a raiz como raíz del árbol, a izquierdo
@@ -20,18 +21,23 @@ class Arbol:
             if derecho is None:
                 self.derecho = Arbol()
             elif not isinstance(derecho, Arbol):
-                    raise TypeError("¡El subárbol derecho debe ser árbol!")
+                raise TypeError("¡El subárbol derecho debe ser árbol!")
             else:
                 self.derecho = derecho
-
 
     def __eq__(self, arbol):
         """Compara dos árboles binarios y devuelve True si son iguales,
         False en otro caso."""
-        return (self.raiz == arbol.raiz
-                and self.izquierdo == arbol.izquierdo
-                and self.derecho == arbol.derecho)
+        return (
+            self.raiz == arbol.raiz
+            and self.izquierdo == arbol.izquierdo
+            and self.derecho == arbol.derecho
+        )
 
+    def repr_aux(self, num):
+        if self.raiz is not None:
+            return f"({self.raiz}\n{'  ' * (num + 1)}{self.izquierdo.repr_aux(num + 1)}\n{'  ' * (num + 1)}{self.derecho.repr_aux(num + 1)})"
+        return "ø"
 
     def __repr__(self):
         """Representación en cadena, legible para humanos, de
@@ -45,52 +51,28 @@ class Arbol:
         ø
         ø)))
         """
-        # def contador_derecho(self, prof):
-        #     # return f"({self.raiz}\n{profundidad(prof+1)}{self.derecho}\n{profundidad(prof+1)}{self.izquierdo})"
-        #     if self is None:
-        #         return prof
-        #     prof += 1
-        #     return contador_derecho(self.derecho, prof)
-        #
-        # def contador_izquierdo(self, prof):
-        #     if self is None:
-        #         return prof
-        #     prof += 1
-        #     return contador_izquierdo(self.izquierdo, prof)
-        #
-        #     return prof
-        #
-        #
-        #
-        # #hola hola
-        #
-        # if self == Arbol():
-        #     return "ø"
-        # #aquí poner profundidad valor
-        # return f"({self.raiz}\n{" "*contador_izquierdo(self,-2)}{self.izquierdo}\n{" "*contador_derecho(self,-2)}{self.derecho})"
-        # #nivel = profundidad(2)
-        # #profundidad = 0 // "  "*2
-        #
-        # return "("+str(self.raiz) +"\n"+ profundidad(2) + repr(self.derecho) +"\n" + profundidad(2) + repr(self.izquierdo) + ")"
+        if self.raiz is None:
+            return "ø"
 
-    #Se comentó en la tarea que esta no sería recursiva.
-    #Al no pedir más que True si es vacío, notamos que de serlo su raíz en el más alto nivel es None, de modo que
-    #solo es necesario un chequeo. Si es el caso, el árbol es necesariamente vacío. En cualquier otro caso
-    #regresa False
+        return self.repr_aux(0)
+
+    # Se comentó en la tarea que esta no sería recursiva.
+    # Al no pedir más que True si es vacío, notamos que de serlo su raíz en el más alto nivel es None, de modo que
+    # solo es necesario un chequeo. Si es el caso, el árbol es necesariamente vacío. En cualquier otro caso
+    # regresa False
     def es_vacio(self):
         """Devuelve True si el árbol es vacío, y False en otro caso."""
         if self.raiz is None:
             return True
         return False
 
-    #También se comentó que esta no sería recursiva en la asignación. Nuevamente consideramos que es así ya que
-    #solo es necesario un chequeo para poder determinar si un árbol consiste en una sola hoja (un nodo). Así,
-    #es_hoja checa si la raíz de un árbol no es None, en cuyo caso tiene raíz. Entonces solo checa si sus hijos
-    #son vacíos, y solo si ambos lo son devuelve True. En otro caso es un árbol vacío y regresa False al no ser
-    #una hoja.
+    # También se comentó que esta no sería recursiva en la asignación. Nuevamente consideramos que es así ya que
+    # solo es necesario un chequeo para poder determinar si un árbol consiste en una sola hoja (un nodo). Así,
+    # es_hoja checa si la raíz de un árbol no es None, en cuyo caso tiene raíz. Entonces solo checa si sus hijos
+    # son vacíos, y solo si ambos lo son devuelve True. En otro caso es un árbol vacío y regresa False al no ser
+    # una hoja.
     def es_hoja(self):
-        """Devuelve True si el árbol tiene un único nodo, y False en otro caso.
-        """
+        """Devuelve True si el árbol tiene un único nodo, y False en otro caso."""
         if self.raiz is not None:
             if self.derecho == Arbol() and self.izquierdo == Arbol():
                 return True
@@ -98,27 +80,25 @@ class Arbol:
                 return False
         return False
 
-
     def copia(self):
         """Devuelve un nuevo árbol idéntico a este."""
         return Arbol()
 
-    #La función es tal que suma 1 por la raíz del árbol y luego cuenta recursivamente los nodos al aplicarse a
-    #los subárboles. De este modo en la recursión se suman todas las raíces (nodos) que se encuentran en los hijos
-    #de las raíces superiores.
+    # La función es tal que suma 1 por la raíz del árbol y luego cuenta recursivamente los nodos al aplicarse a
+    # los subárboles. De este modo en la recursión se suman todas las raíces (nodos) que se encuentran en los hijos
+    # de las raíces superiores.
     def num_nodos(self):
         """Devuelve el número de nodos en el árbol."""
         if self.raiz is None:
             return 0
-        return 1+ self.derecho.num_nodos() + self.izquierdo.num_nodos()
-
+        return 1 + self.derecho.num_nodos() + self.izquierdo.num_nodos()
 
     def direccion(self, elemento):
         """Si elemento se encuentra en el árbol, devuelve cadena con la
         dirección (en binario) del primer nodo del árbol (en un recorrido
         in-order) que contenga al elemento.   En otro caso devuelve False.
         """
-        #Esto no sirve jajaj
+        # Esto no sirve jajaj
         # if self.raiz == elemento:
         #     return "ok"
         # ruta = ""
@@ -127,7 +107,6 @@ class Arbol:
         # if self.izquierdo == elemento:
         #     ruta = ruta +"1"
         # return ruta
-
 
     def gira(self, direccion):
         """Recibe como entrada una dirección dada como una cadena binaria, y gira
@@ -139,18 +118,19 @@ class Arbol:
         """
         return Arbol()
 
-
     def es_isomorfo(self, arbol):
         """Compara dos árboles binarios y devuelve True si son isomorfos,
         False en otro caso.
         """
         return False
 
+
 if __name__ == "__main__":
-    t1=Arbol(3)
-    t2=Arbol(1,t1,t1)
-    t3=Arbol()
-    t4=Arbol(2,t3,t2)
-    #b=Arbol(c,Arbol(),Arbol(d))
-    #print(t4.es_vacio())
+    t1 = Arbol(3)
+    t2 = Arbol(1, t1, t1)
+    t3 = Arbol()
+    t4 = Arbol(2, t3, t2)
+    # b=Arbol(c,Arbol(),Arbol(d))
+    # print(t4.es_vacio())
     print(t4.direccion(2))
+
--- test_arboles.py
+++ test_arboles.py
@@ -2,6 +2,7 @@ import unittest
 import random
 from arboles import *
 
+
 class TestArbol(unittest.TestCase):
     def test_repr_arbol(self):
         """Prueba que la representación de árbol como cadena sea correcta."""
@@ -11,9 +12,9 @@ class TestArbol(unittest.TestCase):
         a = Arbol(2)
         c = "(" + str(a.raiz) + "\n  "
         c += "ø" + "\n  "
-        c += "ø" + ")"        
+        c += "ø" + ")"
         self.assertEqual(str(a), c)
-        a = Arbol('a', Arbol('b', Arbol('c'), Arbol('d')), Arbol('e'))
+        a = Arbol("a", Arbol("b", Arbol("c"), Arbol("d")), Arbol("e"))
         c = "(a\n  (b\n    (c\n      ø\n      ø)\n    (d\n      ø\n      ø))\n  (e\n    ø\n    ø))"
         self.assertEqual(str(a), c)
 
@@ -46,7 +47,9 @@ class TestArbol(unittest.TestCase):
         e = []
         for i in range(6):
             e.append(random.randint(0, 100))
-        a = Arbol(e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], Arbol(e[5]), None))
+        a = Arbol(
+            e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], Arbol(e[5]), None)
+        )
         b = a.copia()
         self.assertIsNot(a, b)
         self.assertEqual(a, b)
@@ -62,9 +65,11 @@ class TestArbol(unittest.TestCase):
         e = []
         for i in range(6):
             e.append(random.randint(0, 100))
-        a = Arbol(e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], Arbol(e[5]), None))
+        a = Arbol(
+            e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], Arbol(e[5]), None)
+        )
         self.assertEqual(a.num_nodos(), 6)
-        
+
     def test_direccion(self):
         """Prueba que la función direccion funcione correctamente."""
         elemento = 42
@@ -84,7 +89,6 @@ class TestArbol(unittest.TestCase):
         # El elemento está en la posición 10
         self.assertEqual(arbol.direccion(elemento), "10")
 
-
     def test_gira(self):
         """Prueba que la función gira funcione correctamente."""
         a = Arbol()
@@ -103,11 +107,21 @@ class TestArbol(unittest.TestCase):
         self.assertIsNot(b, c)
         self.assertEqual(b, c)
         b = a.gira("0")
-        c = Arbol(16, Arbol(izquierdo.raiz, izquierdo.derecho.copia(), izquierdo.izquierdo.copia()), derecho.copia())
+        c = Arbol(
+            16,
+            Arbol(
+                izquierdo.raiz, izquierdo.derecho.copia(), izquierdo.izquierdo.copia()
+            ),
+            derecho.copia(),
+        )
         self.assertIsNot(b, c)
         self.assertEqual(b, c)
         b = a.gira("1")
-        c = Arbol(16, izquierdo.copia(), Arbol(derecho.raiz, derecho.derecho.copia(), derecho.izquierdo.copia()))
+        c = Arbol(
+            16,
+            izquierdo.copia(),
+            Arbol(derecho.raiz, derecho.derecho.copia(), derecho.izquierdo.copia()),
+        )
         self.assertIsNot(b, c)
         self.assertEqual(b, c)
 
@@ -129,10 +143,18 @@ class TestArbol(unittest.TestCase):
         self.assertTrue(a.es_isomorfo(b))
         self.assertTrue(c.es_isomorfo(b))
         self.assertTrue(a.es_isomorfo(a))
-        a = Arbol(e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], Arbol(e[5]), None))
-        b = Arbol(e[0], Arbol(e[4], Arbol(e[5]), None), Arbol(e[1], Arbol(e[2]), Arbol(e[3])))
-        c = Arbol(e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], None, Arbol(e[5])))
-        d = Arbol(e[0], Arbol(e[1], Arbol(e[3]), Arbol(e[2])), Arbol(e[4], None, Arbol(e[5])))
+        a = Arbol(
+            e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], Arbol(e[5]), None)
+        )
+        b = Arbol(
+            e[0], Arbol(e[4], Arbol(e[5]), None), Arbol(e[1], Arbol(e[2]), Arbol(e[3]))
+        )
+        c = Arbol(
+            e[0], Arbol(e[1], Arbol(e[2]), Arbol(e[3])), Arbol(e[4], None, Arbol(e[5]))
+        )
+        d = Arbol(
+            e[0], Arbol(e[1], Arbol(e[3]), Arbol(e[2])), Arbol(e[4], None, Arbol(e[5]))
+        )
         self.assertTrue(a.es_isomorfo(b))
         self.assertTrue(c.es_isomorfo(b))
         self.assertTrue(a.es_isomorfo(a))
